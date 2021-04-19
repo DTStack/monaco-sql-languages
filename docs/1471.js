27dc6b9465788d5a23cface4cf9ff807d6f7b922
xiaowei <wewoor@gmail.com>
2021-04-19 16:34:17 +0800

    build: prettier ignore docs dir

---
+++ docs/1471.js
@@ -0,0 +1,63 @@
+(self.webpackChunkmonaco_languages = self.webpackChunkmonaco_languages || []).push([
+	[1471],
+	{
+		31471: (e, t, n) => {
+			'use strict';
+			n.r(t), n.d(t, { conf: () => s, language: () => o });
+			var s = { comments: { lineComment: '#' } },
+				o = {
+					defaultToken: 'keyword',
+					ignoreCase: !0,
+					tokenPostfix: '.azcli',
+					str: /[^#\s]/,
+					tokenizer: {
+						root: [
+							{ include: '@comment' },
+							[
+								/\s-+@str*\s*/,
+								{
+									cases: {
+										'@eos': { token: 'key.identifier', next: '@popall' },
+										'@default': { token: 'key.identifier', next: '@type' }
+									}
+								}
+							],
+							[
+								/^-+@str*\s*/,
+								{
+									cases: {
+										'@eos': { token: 'key.identifier', next: '@popall' },
+										'@default': { token: 'key.identifier', next: '@type' }
+									}
+								}
+							]
+						],
+						type: [
+							{ include: '@comment' },
+							[
+								/-+@str*\s*/,
+								{
+									cases: {
+										'@eos': { token: 'key.identifier', next: '@popall' },
+										'@default': 'key.identifier'
+									}
+								}
+							],
+							[
+								/@str+\s*/,
+								{
+									cases: {
+										'@eos': { token: 'string', next: '@popall' },
+										'@default': 'string'
+									}
+								}
+							]
+						],
+						comment: [
+							[/#.*$/, { cases: { '@eos': { token: 'comment', next: '@popall' } } }]
+						]
+					}
+				};
+		}
+	}
+]);

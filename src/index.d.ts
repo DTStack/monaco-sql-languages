4d41475c2e4584210796569ad38f2f47fb8cb884
xiaowei <wewoor@gmail.com>
2021-07-15 20:40:06 +0800

    refactor: remove the useless declare for dt-sql-parser

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,6 +3,4 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-declare module 'dt-sql-parser';
-
 declare module '*';

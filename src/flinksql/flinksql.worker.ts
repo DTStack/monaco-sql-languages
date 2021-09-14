b4394303c9ecb5a12a0c3edfdce9d2daeffc36c9
xiaowei <wewoor@gmail.com>
2021-09-14 17:18:46 +0800

    docs: update the preview site

--- src/flinksql/flinksql.worker.ts
+++
@@ -1,9 +0,0 @@
-import { worker } from 'monaco-editor';
-import * as EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js';
-import { FLinkSQLWorker, ICreateData } from './flinkSQLWorker';
-
-self.onmessage = (e: any) => {
-	EditorWorker.initialize((ctx: worker.IWorkerContext, createData: ICreateData) => {
-		return new FLinkSQLWorker(ctx, createData);
-	});
-};

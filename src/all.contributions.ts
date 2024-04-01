0cc6d165595c53eb094776b04b0cad1ac371591c
Hayden <hayden9653@gmail.com>
2024-04-01 10:04:52 +0800

    feat: add all contributions file

---
+++ src/all.contributions.ts
@@ -0,0 +1,9 @@
+import './flinksql/flinksql.contribution';
+import './sparksql/sparksql.contribution';
+import './hivesql/hivesql.contribution';
+import './trinosql/trinosql.contribution';
+import './sql/sql.contribution';
+import './mysql/mysql.contribution';
+import './plsql/plsql.contribution';
+import './pgsql/pgsql.contribution';
+import './impalasql/impalasql.contribution';
--- src/monaco.contribution.ts
+++
@@ -1,9 +0,0 @@
-import './flinksql/flinksql.contribution';
-import './sparksql/sparksql.contribution';
-import './hivesql/hivesql.contribution';
-import './trinosql/trinosql.contribution';
-import './sql/sql.contribution';
-import './mysql/mysql.contribution';
-import './plsql/plsql.contribution';
-import './pgsql/pgsql.contribution';
-import './impalasql/impalasql.contribution';

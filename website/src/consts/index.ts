c1f1fd595e2f98d3e932869adf2f89974fc70e59
Bell <47959147+shiqiWang0@users.noreply.github.com>
2025-10-27 10:46:54 +0800

    Bump/molecule@2.x alpha10 (#190)
    * feat: update molecule@2.0.0-alpha.8
    
    * feat: add welcome
    
    * feat: init colortheme && reset workbench
    
    * feat: add website ui
    
    * feat: improve UI
    
    * feat: imporve workbench to support sidebar
    
    * feat: support problem panel
    
    * feat: support snippetCompletion
    
    * feat: support to register action
    
    * feat: apiDocPage && unitTest init
    
    * feat: improve ui in quick github to visit
    
    * feat(website): support quickStart to create new file && code review
    
    * fix(website): create file to share data
    
    * chore(release): 1.0.0-beta.0
    
    * chore(release): 1.0.0-beta.1
    
    * chore(release): 1.0.0-beta.2
    
    * chore(release): 1.0.0-beta.3
    
    * chore(release): 1.0.0-beta.4
    
    * chore(release): 1.0.0-beta.5
    
    * feat: update demo and others after upgrade monaco-editor's version
    
    * feat: update contribute ts and update unit test
    
    * feat: bump molecule2.0 from alpha.8 to alpha.10 with monaco-editor@0.52.2
    
    ---------
    
    Co-authored-by: beier <beier@dtstack.com>
    Co-authored-by: zhaoge <>

---
+++ website/src/consts/index.ts
@@ -0,0 +1,57 @@
+export const QUICK_GITHUB = 'quick.github';
+
+export const FILE_PATH = 'file.path';
+
+export const PARSE_LANGUAGE = 'parse.language';
+
+export const ACTIVITY_FOLDER = 'activity.folder';
+
+export const ACTIVITY_SQL = 'activity.sql';
+
+export const ACTIVITY_API = 'activity.api';
+
+export const SOURCE_FILE = 'activity.source.file';
+
+export const SOURCE_OUTLINE = 'activity.source.outline';
+
+export const QUICK_GITHUB_HREF = [
+	{
+		href: 'https://github.com/DTStack/dt-sql-parser',
+		title: 'dt-sql-parser'
+	},
+	{
+		href: 'https://github.com/DTStack/monaco-sql-languages',
+		title: 'monaco-sql-languages'
+	},
+	{
+		href: 'https://github.com/DTStack/dt-react-monaco-editor',
+		title: 'dt-react-monaco-editor'
+	}
+];
+
+export const SQL_LANGUAGES = [
+	'HiveSQL',
+	'SparkSQL',
+	'FlinkSQL',
+	'MySQL',
+	'PGSQL',
+	'TrinoSQL',
+	'ImpalaSQL'
+];
+
+export const defaultLanguage = SQL_LANGUAGES[0];
+
+export const defaultEditorTab = {
+	id: `fixedTab`,
+	name: defaultLanguage,
+	data: {
+		value: ``,
+		language: defaultLanguage.toLowerCase()
+	}
+};
+
+export const defaultLanguageStatusItem = {
+	id: `fixedStatusItem`,
+	name: defaultLanguage,
+	sortIndex: 3
+};
